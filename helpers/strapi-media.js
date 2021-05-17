0f33cee8af65a414e2d9c40091a9afc202f9b093
Mads Hougesen <madshougesen@gmail.com>
2021-05-17 17:37:33 +0200

    added fallback image for production

--- helpers/strapi-media.js
+++ helpers/strapi-media.js
@@ -2,6 +2,11 @@ export function getStrapiMedia(url) {
   // Check if URL is a local path
   if (url.startsWith('/')) {
     // Prepend Strapi address
+
+    if (process.env.NODE_ENV === 'production') {
+      return 'https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image.png';
+    }
+
     return `${process.env.API_URL ? process.env.API_URL : 'http://localhost:1337'}${url}`;
   }
   // Otherwise return full URL
