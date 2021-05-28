8680b3e78b4cfeef0353495159348ca28ef85a3b
Mads Hougesen <madshougesen@gmail.com>
2021-05-28 13:51:05 +0200

    disabled placeholder images

--- helpers/strapi-media.js
+++ helpers/strapi-media.js
@@ -3,9 +3,9 @@ export function getStrapiMedia(url) {
   if (url.startsWith('/')) {
     // Prepend Strapi address
 
-    if (process.env.NODE_ENV === 'production') {
+    /*   if (process.env.NODE_ENV === 'production') {
       return 'https://d3aa3603f5de3f81cb9fdaa5c591a84d5723e3cb.hosting4cdn.com/wp-content/uploads/2021/05/fwk-celtic-park.png';
-    }
+    } */
 
     return `${process.env.API_URL ? process.env.API_URL : 'http://localhost:1337'}${url}`;
   }
