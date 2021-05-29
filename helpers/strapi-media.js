45d3657d726e934662744e2e78f890032ebe1753
Mads Hougesen <madshougesen@gmail.com>
2021-05-29 12:19:57 +0200

    Update strapi-media.js

--- helpers/strapi-media.js
+++ helpers/strapi-media.js
@@ -1,13 +1,13 @@
 export function getStrapiMedia(url) {
   // Check if URL is a local path
-  if (url.startsWith('/')) {
+  if (url.startsWith("/")) {
     // Prepend Strapi address
 
-    /*   if (process.env.NODE_ENV === 'production') {
-      return 'https://d3aa3603f5de3f81cb9fdaa5c591a84d5723e3cb.hosting4cdn.com/wp-content/uploads/2021/05/fwk-celtic-park.png';
-    } */
+    if (process.env.NODE_ENV === "production") {
+      return `https://hayat-plakater-backend-gequk.ondigitalocean.app${url}`;
+    }
 
-    return `${process.env.API_URL ? process.env.API_URL : 'http://localhost:1337'}${url}`;
+    return `http://localhost:1337${url}`;
   }
   // Otherwise return full URL
   return url;
