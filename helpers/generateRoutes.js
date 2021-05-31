94beb75fc71bc83163a9fb7252a6ab6dd0712ba1
Mads Hougesen <madshougesen@gmail.com>
2021-05-31 17:21:15 +0200

    added google anayltics

--- helpers/generateRoutes.js
+++ helpers/generateRoutes.js
@@ -3,12 +3,12 @@ import axios from 'axios';
 // http://localhost:1337/products
 
 export default async () => {
-  const baseUrl = process.env.API_URL || 'http://localhost:1337';
+  const baseUrl = process.env.API_URL || 'http://localhost:1337/';
 
-  const routes = ['/'];
+  const routes = ['/', '/plakater', '/about', '/checkout', '/history'];
 
   const products = await axios
-    .get(`${baseUrl}/products`)
+    .get(`${baseUrl}products`)
     .then((res) => res.data)
     .catch((err) => {
       console.log(err);
@@ -18,8 +18,9 @@ export default async () => {
     routes.push(`/produkt/${product.slug}`);
   }
 
+  /* Currently not implemented (in a good way)
   const categories = await axios
-    .get(`${baseUrl}/categories`)
+    .get(`${baseUrl}categories`)
     .then((res) => res.data)
     .catch((err) => {
       console.log(err);
@@ -27,7 +28,7 @@ export default async () => {
 
   for (const category of categories) {
     routes.push(`/kategori/${category.slug}`);
-  }
+  } */
 
   return routes;
 };
